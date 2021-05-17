47a746b0f2f9bfdff53020abb96e58fb2fe9cb3d
rosy <1138440485@qq.com>
2021-05-17 12:01:36 +0800

    将项目提交到版本库

--- src/com/git/TestGitDemo02.java
+++ src/com/git/TestGitDemo02.java
@@ -2,6 +2,6 @@ package com.git;
 
 public class TestGitDemo02 {
     public static void main(String[] args) {
-        System.out.println("这是TestGitDemo02");
+        System.out.println("这是TestGitDemo02 ");
     }
 }
