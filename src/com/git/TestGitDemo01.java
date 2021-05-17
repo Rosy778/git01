7d09d9c0a9a305db15f48f8607a0795439886198
rosy <1138440485@qq.com>
2021-05-17 12:07:16 +0800

    我修改了TestGitDemo01,将项目提交并且推送远程仓库

--- src/com/git/TestGitDemo01.java
+++ src/com/git/TestGitDemo01.java
@@ -3,5 +3,6 @@ package com.git;
 public class TestGitDemo01 {
     public static void main(String[] args) {
         System.out.println("这是TestGitDemo01 ");
+        System.out.println("我修改了TestGitDemo01,将项目提交并且推送远程仓库");
     }
 }
